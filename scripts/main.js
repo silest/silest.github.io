31055c2826f73a063c5114e69f4b4f8de6006efd
silest <67388257+silest@users.noreply.github.com>
2021-11-21 17:04:02 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hallo Welt!';
+
+let silvioHeading = document.querySelector('h2');
+silvioHeading.textContent = 'Hallo Silvio!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/drau.jpg');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Bitte geben Sie Ihren Namen ein.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla ist cool, ' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla ist cool, ' + storedName;
+  }
+
+  myButton.onclick = () => setUserName();
